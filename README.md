3838c448770ea1d9cdc43c0d71d638bb035f2423
shreydan <iamshreydan@gmail.com>
2023-01-02 22:23:01 +0530

    transcript fetch

---
+++ README.md
@@ -0,0 +1,15 @@
+# Youtube In-Video Search
+
+
+### TODO:
+
+- ~~fetch transcript~~
+- ~~combine transcripts into groups~~
+- semantic search on transcripts
+- match results to the time stamp
+- control youtube player based on the time stamp
+
+
+```
+In-progress 🚧️
+```
---
+++ fetch_transcript.py
@@ -0,0 +1,61 @@
+from youtube_transcript_api import YouTubeTranscriptApi, YouTubeRequestFailed
+import regex as re
+from preprocessing import group_sentences
+
+def validate_youtube_link(url: str) -> str:
+    """
+    this method validates the youtube video link provided.
+    input  : url (str)
+    outputs: transcript (string/dict) 
+    """
+    yt_regex = "^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*"
+    matches = re.findall(yt_regex, url)
+    
+    assert len(matches[0][1]) == 11
+
+    video_id:str = matches[0][1]
+
+    return video_id
+
+
+def zip_transcript(transcript:list) -> list:
+    start_times = []
+    texts = []
+    for item in transcript:
+        start_times.append(item['start'])
+        texts.append(item['text'].strip().replace('\n',' '))
+    
+    return list(zip(start_times,texts))
+
+
+def full_text(transcript: list) -> str:
+    texts = []
+    for item in transcript:
+        texts.append(item['text'])
+    return ' '.join(texts).strip()
+
+
+def fetch_transcript(url: str) -> list:
+    
+    video_id = validate_youtube_link(url)
+    
+    try:
+        transcript:list = YouTubeTranscriptApi.get_transcript(video_id=video_id)
+        # transcript = zip_transcript(transcript)
+        transcript = full_text(transcript)
+    except YouTubeRequestFailed:
+        return None
+    
+    return transcript   
+
+
+
+if __name__ == '__main__':
+    sample = 'https://www.youtube.com/watch?v=1nLHIM2IPRY'
+    transcript = fetch_transcript(url=sample)
+
+    groups = group_sentences(transcript, stride=10)
+    
+    with open('sample_group.txt','w') as f:
+        for group in groups:
+            f.write(f"{group}\n\n")
