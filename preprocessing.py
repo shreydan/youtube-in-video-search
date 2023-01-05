b513cd3cb62c401acb5760b7a78da2bed79aace3
shreydan <iamshreydan@gmail.com>
2023-01-05 15:34:28 +0530

    better sentence striding

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,25 @@
 from sentence_splitter import split_text_into_sentences
 
 
-def group_sentences(text, stride=10):
+def group_sentences(text:str, stride:int=10):
     sentences = split_text_into_sentences(text,language='en')
 
     groups = [sentences[i:i+stride] for i in range(0, len(sentences), stride)]
     groups = [' '.join(group).strip() for group in groups]
     return groups
+
+
+def stride_sentences(texts:list, stride=10):
+    groups = [texts[i:i+stride] for i in range(0, len(texts), stride)]
+    groups = [' '.join(group).strip() for group in groups]
+    return groups
+
+
+def dequestionize(question:str):
+    question = [word for word in question.split() if word.lower() not in ['what','where','how','who','why']]
+    return ' '.join(question).replace('?','').strip()
+
+
+def create_similarity_text(question:str, answer: str):
+    question = dequestionize(question)
+    return f"{answer} {question}"
