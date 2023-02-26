f7af60fda8227285bc0210c28c2d7d2aa0eb9c6f
shreydan <iamshreydan@gmail.com>
2023-02-26 22:21:48 +0530

    all tests

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from model import Engine
-from fetch_transcript import fetch_transcript, zip_transcript
-from preprocessing import create_similarity_text, stride_sentences
+from fetch_transcript import fetch_transcript
+from preprocessing import create_similarity_text, create_result_url
 
 
 
@@ -9,10 +9,7 @@ if __name__ == '__main__':
     sample2 = 'https://www.youtube.com/watch?v=1nLHIM2IPRY'
     sample3 = 'https://www.youtube.com/watch?v=nIoXOplUvAw'
     transcript = fetch_transcript(url=sample3)
-    _,transcript = zip_transcript(transcript)
-    groups = stride_sentences(transcript,stride=10)
-    print(groups)
-    model = Engine(groups=groups)
+    model = Engine(transcript)
     
     q = 'hotkey to focus object'
     ans = model.ask(q)
@@ -21,7 +18,6 @@ if __name__ == '__main__':
     print(ans)
     similarity_text = create_similarity_text(q,ans)
     print(similarity_text)
-
-    indices = model.find_similar(similarity_text)
-    print(f"{groups[indices[0]-1]} --- {groups[indices[0]]} --- {groups[indices[0]+1]}")
-    
+    groups,timestamps = model.find_similar(similarity_text)
+    print(groups[0])
+    print(create_result_url(sample3,timestamps[0]))
